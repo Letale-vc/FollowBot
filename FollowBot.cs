7258bc144fe8db56a15bd5801473184c7eeda306
Helpless <letalerv@gmail.com>
2024-05-27 19:58:26 +0300

    change version

--- FollowBot.cs
+++ FollowBot.cs
@@ -448,7 +448,7 @@ namespace FollowBot
         public string Name => "FollowBot";
         public string Author => "NotYourFriend, origial code from Unknown";
         public string Description => "Bot that follow leader.";
-        public string Version => "0.0.6.8";
+        public string Version => "0.0.6.9";
         public UserControl Control => _gui ?? (_gui = new FollowBotGui());
         public JsonSettings Settings => FollowBotSettings.Instance;
         public override string ToString() => $"{Name}: {Description}";
